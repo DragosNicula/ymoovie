63f76aadd9615d50e4fa687017bc2b71e9f092f3
Nicula Dragos <110388376+DragosNicula@users.noreply.github.com>
2025-10-21 18:14:42 +0300

    123
    123

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import cors from "cors";
-import movieRoutes  from "./routes/movieRoutes";
-import { errorHandler } from "./middleware/errorHandler";
-import { PORT } from "./utils/config.js";
+import movieRoutes from "./routes/movieRoutes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-app.use(cors({origin: "http://localhost:3000" }));
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://ymoovie.vercel.app" 
+  ]
+}));
 
 app.use("/movies", movieRoutes);
 app.use(errorHandler);
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`✅ The server is running on port: ${PORT}`);
-})
+  console.log(`✅ The server is running on port: ${PORT}`);
+});
